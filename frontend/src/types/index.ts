e6c151c3d59f5873a260f714fb3674a4cfbe41a8
DAUVERGNE Florian <florian.dauv@gmail.com>
2026-03-08 10:26:50 +0100

    feat:moved types to multiple files

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,24 +1,3 @@
-export type Status = "online" | "offline" | "typing";
-
-export interface Contact {
-  id: string;
-  name: string;
-  avatar?: string;
-  lastMessage: string;
-  time: string;
-  unread?: number;
-  status?: Status;
-}
-
-export interface Message {
-  id: string;
-  content: string;
-  sender: "me" | "other";
-  time: string;
-}
-
-export interface ChatHeaderProps {
-  name: string;
-  avatar?: string;
-  status?: Status;
-}
+export type { Status } from "./status"
+export type { Contact } from "./contact"
+export type { Message } from "./message"
